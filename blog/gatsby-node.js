e17569517e232feea5c5fb4dcd7e84eb232afd53
wonke-dev <wonk0311@gmail.com>
2025-05-14 18:37:42 +0900

    section01. gatsby setting

---
+++ blog/gatsby-node.js
@@ -0,0 +1,26 @@
+/**
+ * Implement Gatsby's Node APIs in this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-node/
+ */
+
+/**
+ * @type {import('gatsby').GatsbyNode['createPages']}
+ */
+const path = require("path")
+
+// Setup Import Alias
+exports.onCreateWebpackConfig = ({ getConfig, actions }) => {
+  const output = getConfig().output || {}
+
+  actions.setWebpackConfig({
+    output,
+    resolve: {
+      alias: {
+        components: path.resolve(__dirname, "src/components"),
+        utils: path.resolve(__dirname, "src/utils"),
+        hooks: path.resolve(__dirname, "src/hooks"),
+      },
+    },
+  })
+}
